8944bd905435c86cef70bbe321562c10f3ae8cfd
Weijia <xiao.wei@northeastern.edu>
2024-08-11 21:23:06 -0400

    added chat history storing/retrieving, and create new bot features

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import { styled, useTheme } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateRoundedIcon from '@mui/icons-material/CreateRounded';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';  
@@ -20,8 +20,9 @@ import MenuItem from '@mui/material/MenuItem';
 
 import { useRouter} from 'next/navigation';
 import {signOut} from "firebase/auth";
-import { auth } from './firebase/config';
+import { auth, firestore } from './firebase/config';
 import {useAuthState} from 'react-firebase-hooks/auth';
+import { collection, doc, getDoc, getDocs, setDoc, query, orderBy, limit } from "firebase/firestore";
 
 const drawerWidth = 240;
 
@@ -71,22 +72,110 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 const RecentChats = ['chat1', 'chat2', 'chat3', 'chat4', 'chat5', 'chat6', 'chat7', 'chat8', 'chat9', 'chat10'];
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 
 export default function Home() {
 
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
 
-  const [messages, setMessages] = useState([
-    {
-      role: 'bot',
-      content: "Hi! I'm the support agent. How can I help you today?",
-      timestamp: Date.now()
-    },
-  ]);
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const [currentBot, setCurrentBot] = useState('main');
+  const [userId, setUserId] = useState('');
+  const [bots, setBots] = useState(['main']);
+  const [openNewBotForm, setOpenNewBotForm] = useState(false);
+  const [botName, setBotName] = useState('');
+  const [botDescription, setBotDescription] = useState('');
+  const [botPrompt, setBotPrompt] = useState('');
+
+  // chat history database structure: /users/{userId}/bots/{botName}/chats/{messageId}
+  const saveChatHistory = async(chatMessages) => {
+    for (const message of chatMessages) {
+      const userChatRef = doc(firestore, 'users', userId, 'bots', currentBot, 'chats', `message-${message.timestamp}`);
+      setDoc(userChatRef, message);
+    }
+  }
+
+  // bot prompt is stored in /users/{userId}/bots/{currentBot}/prompt
+  const saveBotPrompt = async(botName, botDescription) => {
+    const botRef = doc(firestore, 'users', userId, 'bots', botName);
+    const prompt = 'Your name is ' + botName + ' and your role description is: ' + botDescription
+    setBotPrompt(prompt)
+    setDoc(botRef, {prompt: prompt});
+  }
+
+  const getBotPrompt = async(bot) => {
+    const botRef = doc(firestore, 'users', userId, 'bots', bot);
+    const botSnap = await getDoc(botRef);
+    setBotPrompt(botSnap.exists() ? botSnap.data().prompt : '');
+  }
+
+  useEffect(() => {
+    if (user) {
+      setUserId(auth.currentUser.uid);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const defaultIntro = [
+      {
+        role: 'bot',
+        content: `Hi there! I'm ${currentBot === "main"? "your bot" : currentBot}. What do you want to talk about today?`,
+        timestamp: Date.now()
+      },
+    ];
+    const fetchChatHistory = async () => {
+      if (userId != '') {
+        try {
+          const chatRef = collection(firestore, 'users', userId, 'bots', currentBot, 'chats');
+          // TODO: use limit(10) to limit the intial number of messages retrieved, retrieve more when user scroll up
+          const q = query(chatRef, orderBy('timestamp'));
+          const querySnapshot = await getDocs(q);
+          if (!querySnapshot.empty) {
+            const existingMessages = querySnapshot.docs.map(doc => doc.data());
+            setMessages(existingMessages);
+          } else {
+            setMessages(defaultIntro);
+            saveChatHistory(defaultIntro);
+          }
+        } catch (error) {
+          console.error("Error fetching chat history: ", error);
+          // fallback
+          setMessages(defaultIntro);
+          saveChatHistory(defaultIntro);
+        }
+      }
+    };
+
+    fetchChatHistory();
+  }, [userId, currentBot]);
+
+  useEffect(() => {
+    const fetchBotsList = async () => {
+      try {
+        // Reference to the bots collection for a specific user
+        const botsCollectionRef = collection(firestore, 'users', userId, 'bots');
+        
+        // Fetch all documents in the bots collection
+        const snapshot = await getDocs(botsCollectionRef);
+    
+        if (!snapshot.empty) {
+          // Extract bot names from the document IDs
+          const botNames = snapshot.docs.map(doc => doc.id);
+          setBots(botNames)
+        }
+      } catch (error) {
+        console.error('Error getting bot names:', error);
+      }
+    }
+    if (userId != '') {
+      fetchBotsList();
+    }
+  }, [userId]);
 
   if (loading) {
     return (
@@ -105,7 +194,7 @@ export default function Home() {
 
   if (!user) {
     return router.push('/sign-in');
-  }
+  } 
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -116,39 +205,50 @@ export default function Home() {
     });
   }
 
-
-  const sendMessage = async () => {
+  const sendMessage = async (message) => {
     setMessage('');
+    const newMessages = [
+      { role: 'user', content: message, timestamp: Date.now() },
+      { role: 'bot', content: '' },
+    ]
     setMessages((messages) => [
       ...messages,
-      { role: 'user', content: message },
-      { role: 'bot', content: '' },
+      ...newMessages,
     ]);
     const response = fetch('/api/chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+      body: JSON.stringify([
+        { 
+          role: "system", 
+          content: botPrompt,
+        },
+        ...messages, 
+        { role: 'user', content: message }]),
     }).then(async (res) => {
       const reader = res.body.getReader();
       const decoder = new TextDecoder();
 
-      let result = '';
       return reader.read().then(function processText({ done, value }) {
         if (done) {
-          return result;
+          saveChatHistory(newMessages)
+          return;
         } else {
           const text = decoder.decode(value || new Int8Array(), { stream: true });
           setMessages((messages) => {
             const lastMessage = messages[messages.length - 1];
             const priorMessages = messages.slice(0, messages.length - 1);
+            const updatedLastMessage = {
+              ...lastMessage,
+              content: lastMessage.content + text,
+              timestamp: Date.now() 
+            }
+            newMessages[newMessages.length - 1] = updatedLastMessage;
             return [
               ...priorMessages,
-              {
-                ...lastMessage,
-                content: lastMessage.content + text,
-              }
+              updatedLastMessage
             ];
           });
           return reader.read().then(processText);
@@ -156,9 +256,6 @@ export default function Home() {
       });
     });
   };
-
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -168,8 +265,6 @@ export default function Home() {
     setOpen(false);
   };
 
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -177,6 +272,21 @@ export default function Home() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const handleOpenNewBotForm = () => {
+    setOpenNewBotForm(true)
+  }
+
+  const handleCloseNewBotForm = () => {
+    setOpenNewBotForm(false)
+  }
+
+  const settings = [
+    {option: 'Profile', handler: handleCloseUserMenu }, 
+    {option: 'Account', handler: handleCloseUserMenu }, 
+    {option: 'Dashboard', handler: handleCloseUserMenu }, 
+    {option: 'Logout', handler: handleSignOut }
+  ];
 
   return (
     <Box
@@ -206,7 +316,6 @@ export default function Home() {
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ flexGrow: 0 }}>
-          <Button variant="contained" onClick={handleSignOut}>Sign Out</Button>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="User Profile" src="/static/images/avatar/2.jpg" />
@@ -228,9 +337,9 @@ export default function Home() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {settings.map(({option, handler}) => (
+                <MenuItem key={option} onClick={handler}>
+                  <Typography textAlign="center">{option}</Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -268,9 +377,16 @@ export default function Home() {
           margin="0 auto"
         >
           <Stack width="auto" height="auto" spacing={2} overflow='auto'>
-            {RecentChats.map((text) => (
-              <Button key={text} variant="contained" sx={{ width: '100%' }}>
-                {text}
+            {bots.map((bot) => (
+              <Button 
+                key={bot} 
+                variant="contained" 
+                onClick={() => {
+                  setCurrentBot(bot);
+                  getBotPrompt(bot);
+                }} 
+                sx={{ width: '100%' }}>
+                {bot}
               </Button>
             ))}
           </Stack>
@@ -288,14 +404,6 @@ export default function Home() {
         spacing={3}
         mb={8}
       >
-        <Button
-          variant="contained" 
-          color="primary"
-          startIcon={<CreateRoundedIcon />}
-          onClick={() => window.location.reload()}
-        >
-          Start a new chat
-        </Button>
         <Stack
           direction='column'
           spacing={2}
@@ -323,14 +431,114 @@ export default function Home() {
           <TextField
             label='Your message'
             fullWidth
+            multiline
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(k) => {
+              if (k.key === 'Enter') {
+                event.preventDefault();
+                sendMessage(message);
+              }
+            }}
           />
           <Button variant="contained" onClick={sendMessage}>
             <SendRoundedIcon />
           </Button>
         </Stack>
       </Stack>
+      <Button
+          variant="contained" 
+          color="primary"
+          startIcon={<CreateRoundedIcon />}
+          onClick={handleOpenNewBotForm}
+        >
+          Chat with a new bot
+        </Button>
+      <Modal open={openNewBotForm} onClose={handleCloseNewBotForm}>
+        <StyledPopUP 
+          position='absolute' 
+          top='50%' 
+          left='50%' 
+          width='50%'
+          bgcolor='white' 
+          border='1px solid ' 
+          boxShadow={24} 
+          display={'flex'} 
+          flexDirection={'column'} 
+          alignItems={'center'}
+          p = {5}
+          gap={3}
+          sx={{
+            transform: 'translate(-50%, -50%)',
+            borderRadius: '15px'
+          }}>
+            <Typography variant="h6"> Create Your New Bot </Typography>
+            <Stack width='100%' direction='row' spacing={2} alignItems={'center'} display={'flex'} justifyContent={'center'}>
+              <Box width={200}>
+                <Typography variant="p"> Bot Name : </Typography>
+              </Box>
+            
+              <TextField
+                variant='outlined'
+                // fullWidth
+                size="small"
+                label="Name your bot"
+                placeholder="Enter name here ..."
+                sx={{ width: '25ch' }}
+                value={botName}
+                onChange={(e) => {
+                  setBotName(e.target.value)
+                }}
+              />
+            </Stack>
+            <Stack width='100%' direction='row' spacing={2} alignItems={'flex-start'} display={'flex'} justifyContent={'center'}>
+              <Box width={200}>
+                <Typography variant="p"> Role Description : </Typography>
+              </Box>
+            
+              <TextField
+                variant='outlined'
+                fullWidth
+                multiline
+                label="How do you want your bot to be? "
+                placeholder="Enter description here ..."
+                sx={{ width: '25ch' }}
+                value={botDescription}
+                rows={4}
+                onChange={(e) => {
+                  setBotDescription(e.target.value)
+                }}
+              />
+            </Stack>
+            <Button
+                variant="outlined"
+                color='primary'
+                disabled={botName === ''}
+                onClick={()=>{
+                  bots.push(botName);
+                  setCurrentBot(botName);
+                  saveBotPrompt(botName, botDescription);
+                  setBotName('');
+                  setBotDescription('');
+                  handleCloseNewBotForm();
+                }}
+              >
+                Meet Your New Bot!
+            </Button>
+        </StyledPopUP>
+      </Modal>
     </Box>
   );
 }
+
+const StyledPopUP = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down("lg")]: {
+    width: '50vw',
+  },
+  [theme.breakpoints.down("md")]: {
+    width: '80vw',
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: '100vw',
+  }
+}));
